0f3c081cbb6e3eabebc2b595eaa74c8e91d7917b
Oliwier Kulczycki <oleknuso282@gmail.com>
2025-12-04 22:08:37 +0000

    fixed linux awful nvim
    still dont know what caused it

--- .bashrc
+++ .bashrc
@@ -1,7 +1,20 @@
 #!/bin/bash
 # Oliwier Kulczycki
+source $HOME/scripts/colours.sh
+export CLICOLOR=1
 
-fastfetch
-~/scripts/fetchAliases.sh
+# Optional Fastfetch
+# fastfetch
+alias ff='fastfetch'
+alias nf='neofetch'
 
 
+alias vi=nvim
+alias vim=nvim
+alias ls='ls -G'
+alias ll='ls -lG'
+alias lla='ls -laG'
+
+# Clear any colour just in case.
+echo -e "${Color_Off}"
+clear
--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+# Only if it were the same for both systems
+#ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
 OS="$(uname -s)"
 
-
-
 # Only on MacOS
 if [ "$OS" == "Darwin" ]; then
-	ln -sf ~/dotfiles/.zshrc ~/.zshrc
+	ln -sf ~/dotfiles/.bashrc ~/.zshrc
+	ln -sf ~/dotfiles/kitty/kitty-macbook.conf ~/.config/kitty/kitty.conf
 fi
 
 # Only on Linux
 if [ "$OS" == "Linux" ]; then
 	ln -sf ~/dotfiles/.bashrc ~/.bashrc
+	ln -sf ~/dotfiles/kitty/kitty-linux.conf ~/.config/kitty/kitty.conf
 fi
